a590919c97da0cd03b6533ca0bbe81360060f1d6
Emmanuel Silva <emanuelsilvadiaz03@gmail.com>
2024-11-17 05:00:35 -0500

    Adding SameSite attribute to auth cookie

--- game2048-backend/src/auth/auth.controller.ts
+++ game2048-backend/src/auth/auth.controller.ts
@@ -23,6 +23,7 @@ export class AuthController {
 			res.cookie('authentication', token, {
 				httpOnly: true,
 				secure: process.env.NODE_ENV === 'production',
+				sameSite: 'none',
 				expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
 			});
 
